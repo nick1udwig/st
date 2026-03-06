89d268d71c4568e31c8b79cfc837f2da0cf4c318
Nick Ludwig <hosted-fornet@proton.me>
2026-03-06 19:46:48 +0000

    add ci for auto-release

--- cmd/st/main.go
+++ cmd/st/main.go
@@ -7,9 +7,12 @@ import (
 	"github.com/ttstt/st/internal/cli"
 )
 
+var version = "dev"
+
 func main() {
 	app := cli.New(os.Stdout, os.Stderr)
 	cmd := app.RootCommand()
+	cmd.Version = version
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
